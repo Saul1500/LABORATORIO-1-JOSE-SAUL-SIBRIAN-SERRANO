d4af9a7b1078977703352cdd41c24e4c88822b89
Saul1500 <125324281+Saul1500@users.noreply.github.com>
2023-03-01 19:25:50 -0600

    Add files via upload

---
+++ Laboratorio1/Laboratorio1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Laboratorio1.Entidades;
+using Laboratorio1.Negocio;
+
+
+
+ClsPersona Clspersona = new ClsPersona();
+Persona persona = new Persona();
+
+Console.WriteLine("===============================================");
+Console.WriteLine("Ingrese su Nombre: ");
+persona.nombre = (Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su edad: ");
+persona.edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su Sexo: ");
+persona.sexo = (Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su Peso: ");
+persona.peso = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingrese su Altura: ");
+persona.altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(" ");
+Console.WriteLine("===============================================");
+
+Clspersona.PesodePersonas(persona);
+
+
+Console.WriteLine(" ");
+
+Clspersona.MayordeEdad(persona);
+
+
+Console.ReadKey();
+
